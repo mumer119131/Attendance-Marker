a0495a36115d50aea1eecf4762d1f59b961e6912
Muhammad <mumer119131@gmail.com>
2021-03-17 17:11:02 +0500

    HopeFully Done

--- main.py
+++ main.py
@@ -1,97 +1,62 @@
 import openpyxl
 import numpy as np
-import getpass
 import PySimpleGUI as sg
-from datetime import date
-import time
+import xlsxwriter
+agList = {"2019-ag-6051": "FATIMA BASHIR", "2019-ag-6052": "ANIQA FAYYAZ", "2019-ag-6053": "AHTISHAM", "2019-ag-6054": "MUHAMMAD ABUBAKR", "2019-ag-6055": "JAHANZAIB BABAR", "2019-ag-6056": "MUHAMMAD SHOAIB AKBAR", "2019-ag-6057": "KHADIJAH RASOOL", "2019-ag-6058": "ANSA SALEEM", "2019-ag-6059": "ZUHA QAMMAR", "2019-ag-6060": "SYED ASAD ALI BUKHARI", "2019-ag-6061": "MUHAMMAD USAMA SHABIR", "2019-ag-6062": "MUHAMMADSHAHZAIB", "2019-ag-6063": "BUSHRA HAMEED", "2019-ag-6065": "HINA FAROOQ", "2019-ag-6066": "ASIMA SHABBIR", "2019-ag-6067": "MUHAMMAD KHAWAR AZEEM", "2019-ag-6068": "MUHAMMAD AHTESHAM SARWAR", "2019-ag-6069": "MAIRA", "2019-ag-6070": "MAKARAM TAYYAB", "2019-ag-6071": "ABDULLAH ALTAF", "2019-ag-6072": "TALHA AZEEM", "2019-ag-6073": "MUHAMMAD NOMAN", "2019-ag-6074": "TASBIHA TANVEER", "2019-ag-6075": "SHAFA ZAMAN", "2019-ag-6076": "ZOHA USMAN", "2019-ag-6077": "MISHAL JAVAID", "2019-ag-6078": "NABEEL UR REHMAN", "2019-ag-6079": "MUHAMMAD ZAIN", "2019-ag-6080": "LAIBA", "2019-ag-6081": "MUHAMMAD UMER", "2019-ag-6082": "MOHAMMAD RUMAN WARIS", "2019-ag-6084": "SAMIA TANVEER", "2019-ag-6085": "MANAL KHALID", "2019-ag-6086": "HAFIZA KHADIJA SULEMAN", "2019-ag-6087": "MUHAMMAD HASSAN KHALID", "2019-ag-6088": "AASHIR NADEEM", "2019-ag-6089": "MUHAMMAD HAMZA IMRAN BAJWA", "2019-ag-6090": "ADEEL AHMAD", "2019-ag-6091": "MUHAMMAD ABUBAKAR", "2017-ag-7217": "RANA TALHA", "2017-ag-7297":"Ahmed Raza","2019-ag-6137":"Aneebb Hassan","2018-ag-8240":"Usama Shahbaz"}
+dataFromFile = []
+chatListDir = ''
+fileName = ''
+sg.theme("DarkBrown1")
+layout = [[sg.Text("Enter the File Name               "), sg.InputText()],
+          [sg.Text("Choose the Chat File:            "), sg.Input(),sg.FileBrowse(key="-IN-")],
+          [sg.Text("Choose the Output Directery: "), sg.Input(),sg.FolderBrowse(key="-OUT-")],
+          [sg.Button("Submit")]
+          ]
 
+window = sg.Window('Attendance Checker', layout, size=(600, 150))
 
-agList=["2019-ag-6051",
-"2019-ag-6052",
-"2019-ag-6053",
-"2019-ag-6054",
-"2019-ag-6055",
-"2019-ag-6056",
-"2019-ag-6057",
-"2019-ag-6058",
-"2019-ag-6059",
-"2019-ag-6060",
-"2019-ag-6061",
-"2019-ag-6062",
-"2019-ag-6063",
-"2019-ag-6065",
-"2019-ag-6066",
-"2019-ag-6067",
-"2019-ag-6068",
-"2019-ag-6069",
-"2019-ag-6070",
-"2019-ag-6071",
-"2019-ag-6072",
-"2019-ag-6073",
-"2019-ag-6074",
-"2019-ag-6075",
-"2019-ag-6076",
-"2019-ag-6077",
-"2019-ag-6078",
-"2019-ag-6079",
-"2019-ag-6080",
-"2019-ag-6081",
-"2019-ag-6082",
-"2019-ag-6084",
-"2019-ag-6085",
-"2019-ag-6086",
-"2019-ag-6087",
-"2019-ag-6088",
-"2019-ag-6089",
-"2019-ag-6090",
-"2019-ag-6091"]
-dataFromFile=[]
-chatListDir=''
-fileName=''
-sg.theme("Dark Blue 3")
-layout = [[sg.Text("Enter the File Name "),sg.InputText()],
-          [sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-")],[sg.Button("Submit")],
-           ]
-
-window = sg.Window('Attendance Checker', layout, size=(600,150))
-    
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event=="Exit":
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
     elif event == "Submit":
-        fileName=values[0]
-        chatListDir=values["-IN-"]
+        fileName = values[0]
+        chatListDir = values["-IN-"]
+        outPutDir=values["-OUT-"]
         break
 
-currentDate=date.today()
-currentTime=time.localtime()
-current_time = time.strftime("%H-%M-%S", currentTime)
-username = getpass.getuser()
-with open("C:/Users/"+username+"/Desktop/"+fileName+".xlsx","w"):
-    pass
 
-with open (chatListDir) as f:
-     dataFromFile=np.loadtxt(f,dtype=str,delimiter="\n").tolist()
-print("path"+"C:/Users/"+username+"/Desktop/"+str(currentDate)+"_"+str(current_time))
-wb = openpyxl.load_workbook('C:/Users/'+username+'/Desktop/'+fileName+'.xlsx')
-ws=wb.active
-n=1
+
+workbook=xlsxwriter.Workbook(outPutDir+'/'+fileName+'.xlsx')
+worksheet=workbook.add_worksheet()
+worksheet.write(0,0,"Ag")
+worksheet.write(0,1,"Name")
+worksheet.write(0,2,"Attendance")
+with open(chatListDir) as f:
+    dataFromFile = np.loadtxt(f, dtype=str, delimiter="\n").tolist()
+
+n = 1
 for i in agList:
-    
-     ws["A"+str(n)]=i
-     n+=1
 
+    worksheet.write(n,0,i)
+    worksheet.write(n,1,agList.get(i))
+    n += 1
+workbook.close()
 
+wb=openpyxl.load_workbook(outPutDir+'/'+fileName+'.xlsx')
+ws=wb.active
 
-
-for i in range(1,40):
+for i in range(2, len(agList)+2):
     for j in dataFromFile:
-        if(ws['A'+str(i)].value==j):
-             ws['B'+str(i)]="P"
-             break
+        
+        agNum=(j[-12:])
+        
+        if(ws['A'+str(i)].value == agNum):
+            ws['C'+str(i)] = "P"
+            break
         else:
-             ws['B'+str(i)]="A"
-                     
-wb.save('C:/Users/Muhammad Umer/Desktop/pytry.xlsx')
+            ws['C'+str(i)] = "A"
+
+wb.save(outPutDir+'/'+fileName+'.xlsx')
+
 
